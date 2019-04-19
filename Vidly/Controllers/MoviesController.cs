52d834ca8a811f29e0b357179cd907e90ec3844e
sd7776 <31454014+sd7776@users.noreply.github.com>
2019-04-19 19:13:56 -0400

    Other stuff

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Hello Kity" };
+
+            //ViewData["Movie"] = movie;
+            return View(movie);
+        }
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Name = "Andhadhoon"},
+                new Movie{Id = 2, Name = "As above so below"}
+
+            };
+        }
+        //Movies
+        /*
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        */
+
+        //Movies/ByReleaseDate
+        [Route("movies/released/{year}/{month:regex(\\d{4}): range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
