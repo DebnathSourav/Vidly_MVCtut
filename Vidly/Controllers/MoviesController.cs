c0c86365cefc8d5fc5195593eb5fe04e0e9831db
sd7776 <31454014+sd7776@users.noreply.github.com>
2019-04-20 01:56:02 -0400

    Added edit movie and edit customer

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Migrations;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -20,32 +36,74 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        //Create new movies
+        public ViewResult New()
         {
-            return new List<Movie>
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
             {
-                new Movie{Id = 1, Name = "Andhadhoon"},
-                new Movie{Id = 2, Name = "As above so below"}
-
+                Genres = genres
             };
-        }
-        //Movies
-        /*
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View("MovieForm", viewModel);
         }
-        */
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+        
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            //If the movie id is zero make a new movie
+            if(movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleasedDate = movie.ReleasedDate;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
 
         //Movies/ByReleaseDate
         [Route("movies/released/{year}/{month:regex(\\d{4}): range(1,12)}")]
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Vidly
+namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+
+        //Display under what name you want to see in the webpage
+        [Display(Name="Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name ="Released Date")]
+        public DateTime ReleasedDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
     }
 }
